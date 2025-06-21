f7fa9e1573a7da47e497fde7a0c8ae5d5cb7c279
Bhawan Panesar <padabhawan@gmail.com>
2025-06-21 14:08:29 -0700

    Restore from cloud

--- htdocs/wp-content/themes/integrity/patterns/header.php
+++ htdocs/wp-content/themes/integrity/patterns/header.php
@@ -20,9 +20,9 @@
 <div class="wp-block-group"><!-- wp:group {"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
 <div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
 <div class="wp-block-group"><!-- wp:group {"style":{"border":{"radius":"6px"},"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}}},"backgroundColor":"primary","layout":{"type":"constrained"}} -->
-<div class="wp-block-group has-primary-background-color has-background" style="border-radius:6px;padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:html -->
-<div class="fa-icon-wrapper vertical-middle"><i class="fas fa-map-marker-alt" style="font-size:24px; color:#ffffff;"></i></div>
-<!-- /wp:html --></div>
+<div class="wp-block-group has-primary-background-color has-background" style="border-radius:6px;padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:image {"id":24,"sizeSlug":"full","linkDestination":"none","className":"vertical-middle"} -->
+<figure class="wp-block-image size-full vertical-middle"><img src="<?php echo esc_url( get_template_directory_uri() . '/images/headlocate.png'); ?>" alt="" class="wp-image-24"/></figure>
+<!-- /wp:image --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"style":{"spacing":{"padding":{"left":"15px"}}},"layout":{"type":"constrained"}} -->
@@ -38,9 +38,9 @@
 
 <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
 <div class="wp-block-group"><!-- wp:group {"style":{"border":{"radius":"6px"},"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}}},"backgroundColor":"primary","layout":{"type":"constrained"}} -->
-<div class="wp-block-group has-primary-background-color has-background" style="border-radius:6px;padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:html -->
-<div class="fa-icon-wrapper vertical-middle"><i class="fas fa-envelope" style="font-size:24px; color:#ffffff;"></i></div>
-<!-- /wp:html --></div>
+<div class="wp-block-group has-primary-background-color has-background" style="border-radius:6px;padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:image {"id":25,"sizeSlug":"full","linkDestination":"none","className":"vertical-middle"} -->
+<figure class="wp-block-image size-full vertical-middle"><img src="<?php echo esc_url( get_template_directory_uri() . '/images/headmail.png'); ?>" alt="" class="wp-image-25"/></figure>
+<!-- /wp:image --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"style":{"spacing":{"padding":{"left":"15px"}}},"layout":{"type":"constrained"}} -->
@@ -56,9 +56,9 @@
 
 <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
 <div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}},"border":{"radius":"6px"}},"backgroundColor":"primary","layout":{"type":"constrained"}} -->
-<div class="wp-block-group has-primary-background-color has-background" style="border-radius:6px;padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:html -->
-<div class="fa-icon-wrapper vertical-middle"><i class="fas fa-phone-alt" style="font-size:24px; color:#ffffff;"></i></div>
-<!-- /wp:html --></div>
+<div class="wp-block-group has-primary-background-color has-background" style="border-radius:6px;padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:image {"id":26,"sizeSlug":"full","linkDestination":"none","className":"vertical-middle"} -->
+<figure class="wp-block-image size-full vertical-middle"><img src="<?php echo esc_url( get_template_directory_uri() . '/images/headphn.png'); ?>" alt="" class="wp-image-26"/></figure>
+<!-- /wp:image --></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"style":{"spacing":{"padding":{"left":"15px"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
@@ -70,23 +70,6 @@
 <p style="margin-top:0;margin-bottom:0;letter-spacing:1px"><a href="tel:904-655-0755"><?php esc_html_e('904-655-0755','integrity'); ?></a></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
-<!-- /wp:group -->
-
-<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|20"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-<div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}},"border":{"radius":"6px"}},"backgroundColor":"primary","layout":{"type":"constrained"}} -->
-<div class="wp-block-group has-primary-background-color has-background" style="border-radius:6px;padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:html -->
-<div class="fa-icon-wrapper vertical-middle"><i class="fas fa-store" style="font-size:24px; color:#ffffff;"></i></div>
-<!-- /wp:html --></div>
-<!-- /wp:group -->
-
-<!-- wp:group {"style":{"spacing":{"padding":{"left":"15px"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group" style="margin-top:0;margin-bottom:0;padding-left:15px"><!-- wp:heading {"level":5,"fontSize":"medium"} -->
-<h5 class="wp-block-heading has-medium-font-size"><?php esc_html_e('Store','integrity'); ?></h5>
-<!-- /wp:heading -->
-
-<!-- wp:paragraph {"style":{"typography":{"letterSpacing":"1px"},"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
-<p style="margin-top:0;margin-bottom:0;letter-spacing:1px"><a href="<?php echo esc_url(get_option('clinic_store_url', '#')); ?>"><?php esc_html_e('Visit Store','integrity'); ?></a></p>
-<!-- /wp:paragraph --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group --></div>
